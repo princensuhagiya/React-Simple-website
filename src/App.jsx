7aa5f06c0fb1aeebbb8f2947d210285f208b73a1
Prince Suhagiya <prince.suhagiya@shunyavkash.com>
2024-04-13 15:36:16 +0530

    useing state dynamic tab and array map

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import { CORE_CONCEPTS } from "./data";
 import Header from "./compontents/Header.jsx";
 import CoreConcept from "./compontents/CoreConcept.jsx";
-
+import TabButton from "./compontents/TabButton.jsx";
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+  function hendleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
   return (
     <div>
       <Header />
@@ -10,15 +16,51 @@ function App() {
         <section id="core-concepts" class="">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conseptItem) => (
+              <CoreConcept {...conseptItem} />
+            ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => hendleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => hendleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => hendleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => hendleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {!setSelectedTopic && <p>Please select a topic. </p>}
+          {setSelectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  <code>{EXAMPLES[selectedTopic].code}</code>
+                </code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
