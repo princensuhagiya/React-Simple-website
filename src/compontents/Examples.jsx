2cf4e46be27b76272a635e7d9dedfebebb49689b
Prince Suhagiya <prince.suhagiya@shunyavkash.com>
2024-04-17 16:46:48 +0530

    complated :)

--- src/compontents/Examples.jsx
+++ src/compontents/Examples.jsx
@@ -1,6 +1,8 @@
 import { EXAMPLES } from "../data.js";
 import TabButton from "../compontents/TabButton.jsx";
 import { useState } from "react";
+import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState("components");
@@ -9,46 +11,51 @@ export default function Examples() {
     console.log(selectedTopic);
   }
   return (
-    <section id="examples">
-      <h2>Examples</h2>
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === "components"}
-          onSelect={() => hendleSelect("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "jsx"}
-          onSelect={() => hendleSelect("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "props"}
-          onSelect={() => hendleSelect("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "state"}
-          onSelect={() => hendleSelect("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      {!setSelectedTopic && <p>Please select a topic. </p>}
-      {setSelectedTopic && (
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </code>
-          </pre>
-        </div>
-      )}
-    </section>
+    <Section title="Examples" id="examples">
+      <Tabs
+        ButtonContainer="menu"
+        button={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => hendleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => hendleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => hendleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => hendleSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {!setSelectedTopic && <p>Please select a topic. </p>}
+        {setSelectedTopic && (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
+    </Section>
   );
 }
---
+++ src/compontents/Tabs.jsx
@@ -0,0 +1,8 @@
+export default function Tabs({ children, button, ButtonContainer }) {
+  return (
+    <>
+      <ButtonContainer>{button}</ButtonContainer>
+      {children}
+    </>
+  );
+}
